e5c25a8b8ab8e50a6ae63f68a4442ee5f681522f
Benjamin <benjamin.scheinhardt@gmail.com>
2018-03-19 21:26:45 +0100

    create default category when creating folder

--- linkslist/views.py
+++ linkslist/views.py
@@ -23,9 +23,20 @@ class FolderAweList(generics.ListCreateAPIView):
     queryset = FolderAwe.objects.all()
     serializer_class = FolderAweSerializer
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        self.createDefaultCategory(response)
+        return response
+
+    def createDefaultCategory(self, response):
+        # TODO on doit surement pouvoir récupérer l'objet FolderAwe avec self sans refaire un appel à la bdd
+        folderid = response.data['id']
+        if folderid is not None:
+            folder = FolderAwe.objects.get(id=folderid)
+            if folder is not None:
+                Category.objects.create(title="Others", folder=folder)
+
 
 class FolderAweDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = FolderAwe.objects.all()
     serializer_class = FolderAweDetailSerializer
-
-
