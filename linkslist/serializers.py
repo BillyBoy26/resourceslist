fe0c1eace12872bf2587b7856f4bbd55504cafb3
Benjamin <benjamin.scheinhardt@gmail.com>
2018-03-28 15:42:49 +0200

    add simple url field in serializer

--- linkslist/serializers.py
+++ linkslist/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
+from urllib.parse import urlparse
 
 from linkslist.models import LinkData, Category, FolderAwe
 
 
 class LinkSerializer(serializers.ModelSerializer):
+    simpleSiteUrl = serializers.SerializerMethodField('getSimpleUrl')
+
+    def getSimpleUrl(self, obj):
+        if obj.siteurl:
+            return urlparse(obj.siteurl).netloc
+        return ''
+
     class Meta:
         model = LinkData
         fields = ('id', 'title', 'description', 'imageurl',
-                  'siteurl', 'sitename', 'category')
+                  'siteurl', 'simpleSiteUrl', 'sitename', 'category')
 
 
 class CategorySerializer(serializers.ModelSerializer):
