fcb07e1f5c3adbd9f303bcbd65fee4225ac85c44
Benjamin <benjamin.scheinhardt@gmail.com>
2018-04-24 16:23:05 +0200

    delete category

--- linkslist/urls.py
+++ linkslist/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^links/$', views.LinkList.as_view()),
     url(r'^links/(?P<pk>[0-9]+)$', views.LinkDetail.as_view()),
-    # url(r'^linksbyidfolder/(?P<pk>[0-9]+)$', views.LinkListByIdFolder.as_view()),
     url(r'^categories/$', views.CategoryList.as_view()),
+    url(r'^categories/(?P<pk>[0-9]+)$', views.CategoryDetail.as_view()),
     url(r'^folders/$', views.FolderAweList.as_view()),
     url(r'^folders/(?P<pk>[0-9]+)$', views.FolderAweDetail.as_view()),
 ]
--- linkslist/views.py
+++ linkslist/views.py
@@ -1,3 +1,4 @@
+from requests import Response
 from rest_framework import generics
 
 from linkslist.models import LinkData, Category, FolderAwe
@@ -17,6 +18,27 @@ class LinkDetail(generics.RetrieveUpdateDestroyAPIView):
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        self.update_children(instance)
+        self.delete_links(instance)
+        return self.destroy(request, *args, **kwargs)
+
+    def delete_links(self, instance):
+        LinkData.objects.filter(category=instance.id).delete()
+
+    def update_children(self, catToDelete):
+        children = Category.objects.filter(parentcat=catToDelete.id)
+        for child in children:
+            child.parentcat = catToDelete.parentcat
+            child.save()
 
 
 class FolderAweList(generics.ListCreateAPIView):
